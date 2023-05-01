656394faf94116b4483cefb2e03b6fe8eda8a996
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-01 09:19:54 -0400

    added blank comment and post js files, and wrote notes for model/index.js

---
+++ models/index.js
@@ -0,0 +1,15 @@
+
+
+//user has many comments
+
+// user has many posts
+
+// post has many comments
+
+// post has one user
+
+//post has many comments
+
+// coment has one user
+
+// coment belongs to one post
