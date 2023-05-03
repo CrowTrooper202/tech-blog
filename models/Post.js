80f3f5899de269c580ef2737c23300a1907dda8d
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-02 21:22:13 -0400

    fixed Model issue

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class Post extends model { }
+class Post extends Model { }
 
 Post.init(
     {
@@ -15,6 +15,11 @@ Post.init(
         description: {
             type: DataTypes.TEXT,
             allowNull: false
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
         },
         user_id: {
             type: DataTypes.INTEGER,
