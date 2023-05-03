941a4f539b39a89cd86200ddbeadc68b71a5e496
CrowTrooper202 <adamb88cke@gmail.com>
2023-05-02 21:14:06 -0400

    fixed config issue

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,4 +1,4 @@
-const sequelize = require('../cong/connection');
+const sequelize = require('../config/connection');
 const { User, Post } = require('../models');
 
 const userData = require('./userData.json');
